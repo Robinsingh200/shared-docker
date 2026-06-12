ee85df1ce65cf25c0e7cf099e4037b6da5daf063
Robin Singh <robinofficial200@gmail.com>
2026-06-12 18:50:21 +0530

    Refactor clonning function for better syntax

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def clonning(string url , string branch){
-  git url: "$url ,branch: $branch"
+def clonning(String url , String branch){
+  git branch: branch , url: url 
   echo "code - done then " 
 }
